38152d3605d1f69daafc929b25cb0e1146a487b5
SeongHyeon Mun <7sonicx@gmail.com>
2025-12-04 19:25:44 +0900

    feat: Configure GitHub Pages deployment with Actions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/F1-Dashboard-2025',
+  assetPrefix: '/F1-Dashboard-2025',
 };
 
 export default nextConfig;
